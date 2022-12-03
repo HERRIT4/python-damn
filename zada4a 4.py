a0ecfce9e8dad66185d8044ccab5f21d9806705d
HERRIT4 <119725509+HERRIT4@users.noreply.github.com>
2022-12-03 18:54:52 +0500

    Add files via upload

---
+++ zada4a 4.py
@@ -0,0 +1,2 @@
+print('Mercury', 'Venus', sep='*', end='!\n')
+print('Mars', 'Jupiter', sep='**', end='?')
---
+++ zada4a 5.py
@@ -0,0 +1,12 @@
+print('a', 'b', 'c', sep='*')
+print('d', 'e', 'f', sep='**', end='')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='\n')
+# переход на новую строку,так как есть \n
+print('m', 'n', 'o', sep='/', end='!')
+print('p', 'q', 'r', sep='1', end='%')
+print('s', 't', 'u', sep='&', end='\n')
+# переход на новую строку,так как есть \n
+print('v', 'w', 'x', sep='%')
+print('y', 'z', sep='/', end='!')
+# получается 5 строк
